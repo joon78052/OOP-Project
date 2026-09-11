3af935b8dce121e1e5814387f5817483ad34f4a3
Hxmza33101 <hkmughal2007@gmail.com>
2026-09-11 19:09:50 +1000

    Wrote code that makes the order.java class relevant in the project, an order now represents a trader's intention to buy or sell a certain dollar amount of a particular stock

--- src/Order.java
+++ src/Order.java
@@ -1,14 +1,39 @@
 public class Order {
 
-    private Trader trader;
-    private Stock stock;
-    private int quantity;
-    private String type;
+    private final Trader trader;
+    private final Stock stock;
+    private final double usd;
+    private final Trade.Type type;
 
-    public Order(Trader trader, Stock stock, int quantity, String type) {
+    public Order(
+            Trader trader,
+            Stock stock,
+            double usd,
+            Trade.Type type
+    ) {
         this.trader = trader;
         this.stock = stock;
-        this.quantity = quantity;
+        this.usd = usd;
         this.type = type;
+    }
+
+    public Trader getTrader() {
+        return trader;
+    }
+
+    public Stock getStock() {
+        return stock;
+    }
+
+    public double getUsd() {
+        return usd;
+    }
+
+    public Trade.Type getType() {
+        return type;
+    }
+
+    public boolean isBuy() {
+        return type == Trade.Type.BUY;
     }
 }
