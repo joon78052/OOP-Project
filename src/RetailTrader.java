cfa2f317ba041a0720420b289553eaea0f251ee5
Hxmza33101 <hkmughal2007@gmail.com>
2026-09-11 19:14:54 +1000

    Added small changes that simulate panic/herding behaviour, buys and sells slightly fluctuate depending on the performance of the stock

--- src/RetailTrader.java
+++ src/RetailTrader.java
@@ -1,11 +1,49 @@
+import java.util.Random;
+
 public class RetailTrader extends Trader {
 
-    public RetailTrader(String name, double cash) {
+    public RetailTrader(
+            String name,
+            double cash
+    ) {
         super(name, cash);
     }
 
     @Override
-    public void makeDecision() {
-        // can be done later
+    public Order makeDecision(
+            Stock stock,
+            Random random
+    ) {
+
+        double buyChance = 0.50;
+
+        if (stock.getChangePercent() > 0) {
+
+            buyChance = 0.60;
+
+        } else if (
+                stock.getChangePercent() < 0
+        ) {
+
+            buyChance = 0.40;
+        }
+
+        boolean buy =
+                random.nextDouble()
+                        < buyChance;
+
+        double usd =
+                20
+                + random.nextDouble()
+                * 280;
+
+        return new Order(
+                this,
+                stock,
+                usd,
+                buy
+                        ? Trade.Type.BUY
+                        : Trade.Type.SELL
+        );
     }
 }
