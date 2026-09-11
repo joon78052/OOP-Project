7ec6132e3a52d263a16f0b49e60c0cfc838bec06
Hxmza33101 <hkmughal2007@gmail.com>
2026-09-11 19:22:21 +1000

    added logic to institutional trader, its unique as it has a chance to buy after a substantial decline.

--- src/InstitutionalTrader.java
+++ src/InstitutionalTrader.java
@@ -1,11 +1,54 @@
-public class InstitutionalTrader extends Trader {
+import java.util.Random;
 
-    public InstitutionalTrader(String name, double cash) {
+public class InstitutionalTrader
+        extends Trader {
+
+    public InstitutionalTrader(
+            String name,
+            double cash
+    ) {
         super(name, cash);
     }
 
     @Override
-    public void makeDecision() {
-        // later as well
+    public Order makeDecision(
+            Stock stock,
+            Random random
+    ) {
+
+        double buyChance = 0.50;
+
+        if (
+                stock.getChangePercent()
+                < -1.0
+        ) {
+
+            buyChance = 0.65;
+
+        } else if (
+                stock.getChangePercent()
+                > 1.0
+        ) {
+
+            buyChance = 0.35;
+        }
+
+        boolean buy =
+                random.nextDouble()
+                        < buyChance;
+
+        double usd =
+                500
+                + random.nextDouble()
+                * 2000;
+
+        return new Order(
+                this,
+                stock,
+                usd,
+                buy
+                        ? Trade.Type.BUY
+                        : Trade.Type.SELL
+        );
     }
 }
